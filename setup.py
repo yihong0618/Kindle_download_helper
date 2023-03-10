62f7f0ff72b2504520c84d14da38c8ce69c950cd
yihong0618 <zouzou0208@gmail.com>
2023-03-10 08:56:11 +0800

    fix: use the same version

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
     author_email="zouzou0208@gmail.com",
     url="https://github.com/yihong0618/kindle_download_helper",
     license="GPL-3.0-or-later",
-    version="1.2.0",
+    version="1.4.0",
     description="Download all your kindle books and `DeDRM` script.",
     long_description="Download all your kindle books and `DeDRM` script.",
     packages=find_packages(),
